9e2f9c938e81a3e5a1ba8aa06a0946d09552c5f4
silko <silkovichmi20001308@gmail.com>
2021-12-07 18:17:59 +0300

    AAA

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('LOL')
-    print('i hate mty life')
-    print('hlp me')
-
-
-if __name__ == '__main__':
-    main()
+print('AAA batareyka')
