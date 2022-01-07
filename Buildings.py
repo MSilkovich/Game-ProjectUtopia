dfd262d89f2a683f2e50e8de2e1e7a2334ca2bb5
penguiiini <90066666+penguiiini@users.noreply.github.com>
2022-01-07 17:39:02 +0300

    Added Farm class and updated name

---
+++ Buildings.py
@@ -0,0 +1,55 @@
+import pygame as pg
+from CONSTS import MYEVENTTYPE_farm
+
+
+class Castle_cl(pg.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y, sprites):
+        super().__init__(sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pg.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pg.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+class Farm(pg.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y, sprites):
+        super().__init__(sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.n = 0
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pg.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pg.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        if self.n < 2:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+            self.n += 1
+        else:
+            global food
+            food += 10
+            pg.time.set_timer(MYEVENTTYPE_farm, 1000)
