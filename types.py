27787d5582721a9f9c1be8f101f055610016e201
MSilkovich <94919759+MSilkovich@users.noreply.github.com>
2022-01-14 18:40:05 +0300

    Create types.py

---
+++ types.py
@@ -0,0 +1,7 @@
+import pygame
+
+type_farm = pygame.USEREVENT + 1
+type_castle = pygame.USEREVENT + 2
+type_res = pygame.USEREVENT + 3
+type_mill = pygame.USEREVENT + 4
+type_days = pygame.USEREVENT + 5
