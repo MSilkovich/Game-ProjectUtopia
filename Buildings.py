fc52e325a84561365e8668984bcdc9a28709eeeb
MSilkovich <94919759+MSilkovich@users.noreply.github.com>
2022-01-23 15:03:42 +0300

    Update Buildings.py

--- Buildings.py
+++ Buildings.py
@@ -1,7 +1,7 @@
 import pygame
 import os
 import sys
-from SETTINGS import *
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -12,7 +12,7 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Farm(pygame.sprite.Sprite):
+class Build(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows, x, y, sprite_group, event):
         super().__init__(sprite_group)
         self.frames = []
@@ -38,69 +38,6 @@ class Farm(pygame.sprite.Sprite):
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.image = self.frames[self.cur_frame]
             self.n += 1
-        else:
-            pygame.time.set_timer(self.type, 0)
-
-
-class Mill(Farm):
-    def __init__(self, sheet, columns, rows, x, y, sprite_group, event):
-        super().__init__(sheet, columns, rows, x, y, sprite_group, event)
-
-    def update(self):
-        if self.n < 2:
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
-            self.n += 1
-        else:
-            pygame.time.set_timer(self.type, 0)
-
-
-class IronMine(Farm):
-    def __init__(self, sheet, columns, rows, x, y, sprite_group, event):
-        super().__init__(sheet, columns, rows, x, y, sprite_group, event)
-
-    def update(self):
-        if self.n < 2:
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
-            self.n += 1
-        else:
-            pygame.time.set_timer(self.type, 0)
-
-
-class IronMine_animation(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y, sprites):
-        super().__init__(sprites)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-        self.builded = False
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                            sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
-
-
-class Quarry(Farm):
-    def init(self, sheet, columns, rows, x, y, sprite_group, event):
-        super().__init__(sheet, columns, rows, x, y, sprite_group, event)
-
-    def update(self):
-        if self.m < 2:
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
-            self.m += 1
         else:
             pygame.time.set_timer(self.type, 0)
 
