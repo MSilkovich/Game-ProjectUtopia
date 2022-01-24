50131ad37453c67e3b537ef086b82dfdc7948b44
MSilkovich <94919759+MSilkovich@users.noreply.github.com>
2022-01-25 00:32:45 +0300

    Update startScreen.py

--- startScreen.py
+++ startScreen.py
@@ -32,17 +32,16 @@ class StartButton:
     def render(self, screen):
         mousePos = pygame.mouse.get_pos()
         if self.mouse_in(mousePos):
-            pygame.draw.rect(screen, (0, 50, 255), (self.position[0], self.position[1], self.width, self.heigth))
+            pygame.draw.rect(screen, (90, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
             if pygame.mouse.get_pressed()[0] and self.mouse_in(mousePos):
+                pygame.draw.rect(screen, (51, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
                 global start, start1
                 start = False
                 start1 = False
-        elif pygame.mouse.get_pressed() and self.mouse_in(mousePos):
-            pygame.draw.rect(screen, (102, 0, 0), (50, 50, 200, 200))
         else:
-            pygame.draw.rect(screen, (102, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
+            pygame.draw.rect(screen, (122, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
         self.font = pygame.font.Font(pygame.font.get_default_font(), 30)
-        valueSurf = self.font.render(f"{self.text}", True, (51, 51, 51))
+        valueSurf = self.font.render(f"{self.text}", True, (255, 255, 255))
         textx = self.position[0] + (self.width / 2) - (valueSurf.get_rect().width / 2)
         texty = self.position[1] + (self.heigth / 2) - (valueSurf.get_rect().height / 2)
         screen.blit(valueSurf, (textx, texty))
@@ -59,8 +58,9 @@ class ConButton(StartButton):
     def render(self, screen):
         mousePos = pygame.mouse.get_pos()
         if self.mouse_in(mousePos):
-            pygame.draw.rect(screen, (0, 50, 255), (self.position[0], self.position[1], self.width, self.heigth))
+            pygame.draw.rect(screen, (90, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
             if pygame.mouse.get_pressed()[0] and self.mouse_in(mousePos):
+                pygame.draw.rect(screen, (51, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
                 global start, food, foodplus, wood, woodplus, stone, stonplus, gold, goldplus, iron, ironres,\
                     population, populationplus, science, scienceplus, happiness, limit, days,\
                     structures_save, structures
@@ -68,14 +68,10 @@ class ConButton(StartButton):
                 for elem in result:
                     structures_save[(elem[4], elem[5])] = (elem[1], elem[2], elem[3], (elem[4], elem[5]))
                 start = False
-
-
-        elif pygame.mouse.get_pressed() and self.mouse_in(mousePos):
-            pygame.draw.rect(screen, (255, 0, 255), (50, 50, 200, 200))
         else:
-            pygame.draw.rect(screen, (0, 0, 255), (self.position[0], self.position[1], self.width, self.heigth))
+            pygame.draw.rect(screen, (122, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
         self.font = pygame.font.Font(pygame.font.get_default_font(), 30)
-        valueSurf = self.font.render(f"{self.text}", True, (255, 0, 0))
+        valueSurf = self.font.render(f"{self.text}", True, (255, 255, 255))
         textx = self.position[0] + (self.width / 2) - (valueSurf.get_rect().width / 2)
         texty = self.position[1] + (self.heigth / 2) - (valueSurf.get_rect().height / 2)
         screen.blit(valueSurf, (textx, texty))
@@ -92,16 +88,15 @@ class BackButton(StartButton):
     def render(self, screen):
         mousePos = pygame.mouse.get_pos()
         if self.mouse_in(mousePos):
-            pygame.draw.rect(screen, (0, 50, 255), (self.position[0], self.position[1], self.width, self.heigth))
+            pygame.draw.rect(screen, (90, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
             if pygame.mouse.get_pressed()[0] and self.mouse_in(mousePos):
+                pygame.draw.rect(screen, (51, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
                 global start1
                 start1 = False
-        elif pygame.mouse.get_pressed() and self.mouse_in(mousePos):
-            pygame.draw.rect(screen, (255, 0, 255), (50, 50, 200, 200))
         else:
-            pygame.draw.rect(screen, (0, 0, 255), (self.position[0], self.position[1], self.width, self.heigth))
+            pygame.draw.rect(screen, (122, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
         self.font = pygame.font.Font(pygame.font.get_default_font(), 30)
-        valueSurf = self.font.render(f"{self.text}", True, (255, 0, 0))
+        valueSurf = self.font.render(f"{self.text}", True, (255, 255, 255))
         textx = self.position[0] + (self.width / 2) - (valueSurf.get_rect().width / 2)
         texty = self.position[1] + (self.heigth / 2) - (valueSurf.get_rect().height / 2)
         screen.blit(valueSurf, (textx, texty))
@@ -117,8 +112,9 @@ class DescriptionButton(StartButton):
     def render(self, screen):
         mousePos = pygame.mouse.get_pos()
         if self.mouse_in(mousePos):
-            pygame.draw.rect(screen, (0, 50, 255), (self.position[0], self.position[1], self.width, self.heigth))
+            pygame.draw.rect(screen, (90, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
             if pygame.mouse.get_pressed()[0] and self.mouse_in(mousePos):
+                pygame.draw.rect(screen, (51, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
                 intro_text = ['Описание:!',
                               'Средневековый город. В центре города - замок. По краям!',
                               'замка - крепостная стена. Вокруг крепостной стены - город.',
@@ -139,10 +135,12 @@ class DescriptionButton(StartButton):
                 global start1
                 start1 = True
                 scr1 = pygame.display.set_mode(siz1)
+                fon1 = pygame.transform.scale(load_image('fon.png'), (siz1[0], siz1[1]))
+                scr1.blit(fon1, (0, 0))
                 for line in intro_text:
                     if line[-1] == '!':
                         line = line.rstrip('!')
-                        string_rendered = font.render(line, 1, (0, 125, 125))
+                        string_rendered = font.render(line, 1, (125, 0, 0))
                         intro_rect = string_rendered.get_rect()
                         text_coord += 10
                         intro_rect.top = text_coord
@@ -150,15 +148,15 @@ class DescriptionButton(StartButton):
                         text_coord += intro_rect.height
                         scr1.blit(string_rendered, intro_rect)
                     else:
-                        string_rendered = font.render(line, 1, (0, 125, 125))
+                        string_rendered = font.render(line, 1, (102, 0, 0))
                         intro_rect = string_rendered.get_rect()
                         text_coord += 10
                         intro_rect.top = text_coord
                         intro_rect.x = 100
                         text_coord += intro_rect.height
                         scr1.blit(string_rendered, intro_rect)
-                btn_start1 = StartButton((100, 450), butHeigth=100, butWidth=200, text='К игре!')
-                btn_back = BackButton((400, 450), butHeigth=100, butWidth=200, text='На главную.')
+                btn_start1 = StartButton((100, 475), butHeigth=100, butWidth=200, text='К игре!')
+                btn_back = BackButton((400, 475), butHeigth=100, butWidth=200, text='На главную.')
                 while start1:
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
@@ -166,12 +164,10 @@ class DescriptionButton(StartButton):
                     btn_start1.render(scr1)
                     btn_back.render(scr1)
                     pygame.display.flip()
-        elif pygame.mouse.get_pressed() and self.mouse_in(mousePos):
-            pygame.draw.rect(screen, (255, 0, 255), (50, 50, 200, 200))
         else:
-            pygame.draw.rect(screen, (0, 0, 255), (self.position[0], self.position[1], self.width, self.heigth))
+            pygame.draw.rect(screen, (122, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
         self.font = pygame.font.Font(pygame.font.get_default_font(), 30)
-        valueSurf = self.font.render(f"{self.text}", True, (255, 0, 0))
+        valueSurf = self.font.render(f"{self.text}", True, (255, 255, 255))
         textx = self.position[0] + (self.width / 2) - (valueSurf.get_rect().width / 2)
         texty = self.position[1] + (self.heigth / 2) - (valueSurf.get_rect().height / 2)
         screen.blit(valueSurf, (textx, texty))
@@ -192,7 +188,7 @@ def terminate():
 
 def start_screen():
     intro_text = ['Проект утопия']
-    size = WIDTH, HEIGHT = 800, 600
+    size = WIDTH, HEIGHT = 900, 600
     screen = pygame.display.set_mode(size)
     fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
     pygame.font.get_fonts()
