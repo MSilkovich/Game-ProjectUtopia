23fdaab2d4b93d3e90070be7183f6042af97c727
MSilkovich <94919759+MSilkovich@users.noreply.github.com>
2022-01-28 18:55:26 +0300

    Create setting_classes.py

---
+++ setting_classes.py
@@ -0,0 +1,36 @@
+import pygame
+from SETTINGS import *
+
+
+class QuitButton:
+    def __init__(self, position: tuple, butHeigth: int = 40, butWidth: int = 150, text: str = "Кнопка"):
+        self.position = position
+        self.width = butWidth
+        self.heigth = butHeigth
+        self.text = text
+
+    def mouse_in(self, mous_pos):
+        x, y = mous_pos
+        px, py = self.position
+        if x > px and x < px + self.width:
+            if y > py and y < py + self.heigth:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def render(self, screen):
+        mousePos = pygame.mouse.get_pos()
+        if self.mouse_in(mousePos):
+            pygame.draw.rect(screen, (90, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
+            if pygame.mouse.get_pressed()[0] and self.mouse_in(mousePos):
+                pygame.draw.rect(screen, (51, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
+                terminate()
+        else:
+            pygame.draw.rect(screen, (122, 0, 0), (self.position[0], self.position[1], self.width, self.heigth))
+        self.font = pygame.font.Font(pygame.font.get_default_font(), 23)
+        valueSurf = self.font.render(f"{self.text}", True, (255, 255, 255))
+        textx = self.position[0] + (self.width / 2) - (valueSurf.get_rect().width / 2)
+        texty = self.position[1] + (self.heigth / 2) - (valueSurf.get_rect().height / 2)
+        screen.blit(valueSurf, (textx, texty))
