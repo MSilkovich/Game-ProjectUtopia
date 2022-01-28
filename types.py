7edfba503d43833b8a5f1fa6bc57339e6f2563af
MSilkovich <94919759+MSilkovich@users.noreply.github.com>
2022-01-28 18:54:40 +0300

    Delete types.py

--- types.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-type_farm = pygame.USEREVENT + 1
-type_castle = pygame.USEREVENT + 2
-type_res = pygame.USEREVENT + 3
-type_mill = pygame.USEREVENT + 4
-type_days = pygame.USEREVENT + 5
