9d60af959e0ae2da3e72267da4216291666d3e5a
ping is king <90066666+penguiiini@users.noreply.github.com>
2022-01-29 22:08:19 +0300

    Update Buildings2.py

--- version02/Buildings2.py
+++ version02/Buildings2.py
@@ -1,6 +1,4 @@
 import pygame
-import os
-import sys
 
 
 class Build(pygame.sprite.Sprite):
@@ -114,3 +112,33 @@ class Castle_cl(pygame.sprite.Sprite):
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
+
+
+class King(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y, sprite_group, event):
+        super().__init__(sprite_group)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.n = 0
+        self.type = event
+
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        if self.n < 4:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+            self.n += 1
+        else:
+            pygame.time.set_timer(self.type, 0)
